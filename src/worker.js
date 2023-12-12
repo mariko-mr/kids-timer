70d1fcf7704c8022c500caa08c98b4a0527515f4
mariko_mr <ma.kakiuchi@gmail.com>
2023-12-12 13:59:45 +0900

    feature: resume時の動作を修正#16

--- src/worker.js
+++ src/worker.js
@@ -1,27 +1,3 @@
-// self.addEventListener("message", (event) => {
-//   if (event.data.type === "start") {
-//     // 初期化
-//     let timerDuration = 0;
-//     let elapsedSeconds = 0;
-
-//     timerDuration = event.data.timerDuration;
-
-//     const updateTimer = () => {
-//       if (elapsedSeconds > timerDuration) {
-//         // タイマー終了の処理
-//         clearInterval(timerInterval);
-//         self.postMessage("timerFinished");
-//       } else {
-//         // タイマー更新の処理
-//         elapsedSeconds++;
-//         self.postMessage({ type: "timerUpdate", elapsedSeconds });
-//       }
-//     };
-
-//     const timerInterval = setInterval(updateTimer, 1000);
-//   }
-// });
-
 // 初期化
 let timerDuration = 0;
 let elapsedSeconds = 0;
@@ -34,7 +10,11 @@ self.addEventListener("message", (event) => {
   } else if (event.data.type === "pause") {
     clearInterval(timerInterval);
   } else if (event.data.type === "resume") {
-    startTimer();
+    if (elapsedSeconds > timerDuration) {
+      return;
+    } else {
+      startTimer();
+    }
   }
 });
 
